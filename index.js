39e23d9a1f211c76dcceb81f233c64609a88b5c2
Hemant Kuralkar <hkuralkar@gmail.com>
2022-07-29 17:34:57 +0530

    Express server created

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3001;
+
+app.get("/",(req, res)=>{
+    res.send("Hello world");
+})
+
+
+app.listen(PORT,()=>{
+
+    console.log(`App started on PORT: ${PORT}`)
+})
